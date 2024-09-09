9722ebfad6743ae2545d4b6dec3008e9032f0d43
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-09-09 22:24:55 +0800

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ export default {
     theme: {
         extend: {
             colors: {
-
+                c_Pink: "hsl(322, 100%, 66%)",
+                c_Very_Pale_Cyan: "hsl(193, 100%, 96%)",
+                c_Very_Dark_Cyan: "hsl(192, 100%, 9%)",
+                c_Grayish_Blue: "hsl(208, 11%, 55%)",
             },
             fontFamily: {
-
+                body: ["Open Sans", "system-ui"],
+                heading: ["Poppins", "sans-serif"]
             }
         },
     },
